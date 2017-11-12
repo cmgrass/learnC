ab6d06c4888c2bd11735232eff0dbf27291b3083
cmgrass <C.grass@live.com>
2017-11-12 15:32:33 -0500

    Improvd my encrypt/decrypt program. There could be more improvements but I must move on.

--- headFirstC/chapter04/encrypt/dataFileHider.c
+++ headFirstC/chapter04/encrypt/dataFileHider.c
@@ -12,6 +12,12 @@ int die_error_argError() {
     exit(1);
 }
 
+int close_file(FILE *myFile) {
+    if (myFile) {
+        fclose(myFile);
+    }
+}
+
 int main(int argc, char *argv[])
 {
 
@@ -20,21 +26,39 @@ int main(int argc, char *argv[])
     }
 
     char optionChar = 0;
+    char fileData[80] = {'\0'};
+    FILE *fileToEncrypt = 0;
+    FILE *fileToDecrypt = 0;
+    FILE *encryptedFile = fopen("encryptedFile.txt", "w");
+    FILE *decryptedFile = fopen("decryptedFile.txt", "w");
 
     while ((optionChar = getopt(argc, argv, "e:d:")) != EOF) {
         switch (optionChar) {
             case 'e':    /* Encrypt mode */
-                encrypt(argv[optind - 1]);
-                printf("%s\n", argv[optind - 1]);
+                fileToEncrypt = fopen(argv[optind - 1], "r");
+                while(fgets(fileData, 80, fileToEncrypt) != NULL) {
+                    encrypt(fileData);
+                    fprintf(encryptedFile, "%s\n", fileData);
+                }
                 break;
+
             case 'd':    /* Decrypt mode */
-                encrypt(argv[optind - 1]);
-                printf("%s\n", argv[optind - 1]);
+                fileToDecrypt = fopen(argv[optind - 1], "r");
+                while(fgets(fileData, 80, fileToDecrypt) != NULL) {
+                    encrypt(fileData);
+                    fprintf(decryptedFile, "%s\n", fileData);
+                }
                 break;
+
             default:
                 die_error_argError();
         }
     }
+
+    close_file(fileToEncrypt);
+    close_file(fileToDecrypt);
+    close_file(encryptedFile);
+    close_file(decryptedFile);
 
     /*
       When getopt() is finisehd processing opions, `optind` returns the index
