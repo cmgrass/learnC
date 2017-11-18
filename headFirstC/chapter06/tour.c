16dcb21e7fe443feb6bf2e011cdd7bdf0aa158f0
cmgrass <C.grass@live.com>
2017-11-18 16:50:48 -0500

    Update island list in chapter06.

--- headFirstC/chapter06/tour.c
+++ headFirstC/chapter06/tour.c
@@ -18,17 +18,20 @@ void print_island(island *curr_island)
 
 island amity = {"Amity", "9:00", "17:00", NULL};
 island craggy = {"Craggy", "9:00", "17:00", NULL};
+island skull = {"Skull", "9:00", "17:00", NULL};
 island isla_nubar = {"Isla Nubar", "9:00", "17:00", NULL};
 island shutter = {"Shutter", "9:00", "17:00", NULL};
 
 int main()
 {
     amity.next = &craggy;
-    craggy.next = &isla_nubar;
+    craggy.next = &skull;
+    skull.next = &isla_nubar; 
     isla_nubar.next = &shutter;
 
     print_island(&amity);
     print_island(&craggy);
+    print_island(&skull); 
     print_island(&isla_nubar);
     print_island(&shutter);
 
