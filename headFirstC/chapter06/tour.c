db8cd7f949bd334c56dc8294889c9ffbfe08427d
cmgrass <C.grass@live.com>
2017-11-18 16:37:27 -0500

    Began using vim, and figuring out recursive structures.

---
+++ headFirstC/chapter06/tour.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+
+/* Build a recursive structure */
+/*    NOTE: Recursive structures require `typedef struct <name>` syntax */
+typedef struct island {
+    char *name;
+    char *open;
+    char *close;
+    struct island *next;
+} island;
+
+void print_island(island *curr_island)
+{
+   printf("Address of %s: %p, next island is at: %p\n",
+          curr_island->name, curr_island, curr_island->next); 
+}
+
+island amity = {"Amity", "9:00", "17:00", NULL};
+island craggy = {"Craggy", "9:00", "17:00", NULL};
+island isla_nubar = {"Isla Nubar", "9:00", "17:00", NULL};
+island shutter = {"Shutter", "9:00", "17:00", NULL};
+
+int main()
+{
+    amity.next = &craggy;
+    craggy.next = &isla_nubar;
+    isla_nubar.next = &shutter;
+
+    print_island(&amity);
+    print_island(&craggy);
+    print_island(&isla_nubar);
+    print_island(&shutter);
+
+    return 0;
+}
