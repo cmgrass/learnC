b5297caf4a107afb9195297f72086fe2b9a0cf96
cmgrass <C.grass@live.com>
2017-11-23 11:31:11 -0500

    Function pointers.

--- headFirstC/chapter07/find.c
+++ headFirstC/chapter07/find.c
@@ -2,6 +2,26 @@
 #include <string.h>
 #include <ctype.h>
 
+int match_sports_no_bieber(char *string_p)
+{
+    return (strstr(string_p, "SPORTS") && !strstr(string_p, "BIEBER"));
+}
+
+int match_theatre_and_movies(char *string_p)
+{
+    return (strstr(string_p, "THEATRE") && strstr(string_p, "MOVIES"));
+}
+
+int match_trucks_or_chess(char *string_p)
+{
+    return (strstr(string_p, "TRUCKS") || strstr(string_p, "CHESS"));
+}
+
+int match_no_workout(char *string_p)
+{
+    return (!strstr(string_p, "WORKING OUT"));
+}
+
 void string_to_upper(char (*string_p) [80], int size)
 {
     int i = 0;
@@ -9,30 +29,29 @@ void string_to_upper(char (*string_p) [80], int size)
     int new_char = 0;
     for (i = 0; i < size; i++) {
         k = 0;
-        printf("START: %i\n", i);
+        /*printf("START: %i\n", i);*/
         while (string_p[i][k]) {
             new_char = toupper(string_p[i][k]);
             string_p[i][k] = new_char;
-            printf("%c", (char)new_char);
+            /*printf("%c", (char)new_char);*/
             k++;
         }
-        puts("");
+        /*puts("");
         printf("%s\n", string_p[i]);
         puts("DONE\n");
+        */
     }
 }
 
 /* find sports fans who don't like bieber */
-void find(char (*string_p) [80], int size)
+void find(int (*match)(char *), char (*string_p) [80], int size)
 {
-    string_to_upper(string_p, size);
-
     int i;
     puts("Search results:");
     puts("-------------------------");
 
     for (i = 0; i < size; i++) {
-        if (strstr(string_p[i], "SPORTS") && !strstr(string_p[i], "BIEBER")) {
+        if (match(string_p[i])) {
             printf("%s\n", string_p[i]);
         }
     }
@@ -52,7 +71,11 @@ int main()
     strcpy(ADS[5], "Josh: SJM likes sports, movies and theatre");
     strcpy(ADS[6], "Jed: DBM likes theatre, books and dining");    
 
-    find(ADS, NUM_ADS);
+    string_to_upper(ADS, NUM_ADS);
+    find(match_sports_no_bieber, ADS, NUM_ADS);
+    find(match_theatre_and_movies, ADS, NUM_ADS);
+    find(match_trucks_or_chess, ADS, NUM_ADS);
+    find(match_no_workout, ADS, NUM_ADS);
 
     return 0;
 }
