ef0b3daf23555237a51202f1e4ca80cbff4330f6
cmgrass <C.grass@live.com>
2017-12-30 19:43:19 -0500

    Finished chapter 9. Learned to use `fork()` as a way to avoid terminating a parent process while calling `exec()`.

--- headFirstC/chapter09/coffee/practice_exec/practice_exec.c
+++ headFirstC/chapter09/coffee/practice_exec/practice_exec.c
@@ -11,13 +11,27 @@ void print_err()
 
 int main(int argc, char *argv[])
 {
-//  if (execl("/usr/bin/python", "/usr/bin/python",
-//      "./test.py", NULL) == -1) {
-//    print_err();
-//    return 1;
-//  }
-//
-//  puts("Well, that was easy. Let's try a tougher one");
+  pid_t pid = fork();
+
+  /* Fork the parent to create a child that can execute our python script */
+  /* This way the parent can continue, because `exec()` functions cause */
+  /* the origin process to terminate. We sacrifice the child. */
+  if (!pid) {
+    /* child active */
+    if (execl("/usr/bin/python", "/usr/bin/python",
+        "./test.py", NULL) == -1) {
+      print_err();
+      return 1;
+    }
+  } else if (pid == -1) {
+    /* error */
+    print_err();
+    return 1;
+  } else {
+    /* parent active */
+  }
+
+  puts("Well, that was easy. Let's try a tougher one");
 
   if (execl("/usr/bin/python", "/usr/bin/python",
       "../../../../../learnPython/ex43/gameMain.py", NULL) == -1) {
